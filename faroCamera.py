e3e74442bc18ea44f73d2ad6dcab8e7c5fb89e9f
mbelgie <mbelgie>
2022-02-15 22:06:32 -0700

    added video method

--- faroCamera.py
+++ faroCamera.py
@@ -14,4 +14,11 @@ class FaroCamera():
     def autoCapturePhoto(self, imageLocation):
         self.camera.capture(imageLocation)
 
-    
+    def autoCaptureVideo(self, videoLocation, videoDuration):
+        self.camera.resolution = (1280, 720)
+        self.camera.start_preview()
+        self.camera.start_recording(videoLocation)
+        sleep(videoDuration)
+        self.camera.stop_recording()
+        self.camera.stop_preview()
+
--- test.py
+++ test.py
@@ -7,8 +7,12 @@ def preview(camera):
 def takePhoto(camera, location):
     camera.autoCapturePhoto(location)
 
+def takeVideo(camera, location, duration):
+    camera.autoCaptureVideo(location, duration)
+
 
 if __name__=="__main__":
     camera = faroCamera.FaroCamera()
     preview(camera)
     takePhoto(camera, "/home/pi/camera/camera/photos/test.jpg")
+    takeVideo(camera, "/home/pi/camera/camera/videos/test.h264", 5)
